db85e41eab93604c50bcf73382cdecf21c8c32bf
Aashir Siddiqui <aashirsiddiqui217@gmail.com>
2026-08-21 20:12:13 +0500

    Change tagName to imageTag in dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,7 +1,7 @@
 def call(Map config = [:]) {
     def imageName = config.get('imageName', 'notes-app')
-    def tagName = config.get('tagName', 'latest')
+    def imageTag = config.get('imageTag', 'latest')
 
-    echo "Building Docker Image: ${imageName}:${tagName}..."
-    sh "docker build -t ${imageName}:${tagName} ."
+    echo "Building Docker Image: ${imageName}:${imageTag}..."
+    sh "docker build -t ${imageName}:${imageTag} ."
 }
