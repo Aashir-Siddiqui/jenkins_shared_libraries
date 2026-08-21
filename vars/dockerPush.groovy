06c62306f62ecf0e6290c2087470906d2dc40a3a
Aashir Siddiqui <aashirsiddiqui217@gmail.com>
2026-08-21 20:11:32 +0500

    Add dockerPush function to push images to Docker Hub

---
+++ vars/dockerPush.groovy
@@ -0,0 +1,18 @@
+def call(Map config = [:]) {
+    def imageName = config.get('imageName', 'notes-app')
+    def imageTag = config.imageTag
+    def credsId   = config.get('credsId', 'docker-hub-creds')
+
+    echo "Pushing ${imageName} to Docker Hub..."
+    withCredentials([usernamePassword(
+        credentialsId: credsId,
+        usernameVariable: 'DOCKER_USER',
+        passwordVariable: 'DOCKER_PASS'
+    )]) {
+        sh """
+            echo "\$DOCKER_PASS" | docker login -u "\$DOCKER_USER" --password-stdin
+            docker tag ${imageName}:${imageTag} \$DOCKER_USER/${imageName}:${imageTag}
+            docker push \$DOCKER_USER/${imageName}:${imageTag}
+        """
+    }
+}
